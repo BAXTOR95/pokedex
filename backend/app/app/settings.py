2d6b2caa9748e6ea453f504dc5fc72e08698e0c5
Brian Arriaga <brian.arriaga@gmail.com>
2021-04-15 13:19:46 -0400

    Configured backend for deployment

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(=4f(sc9=8nzi9z(fp8*r+lsw4a!2jkdqj4z5cd+5v53!_(jf!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -135,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
 
 # changes the in-build User model to ours (Custom)
 AUTH_USER_MODEL = 'core.User'
